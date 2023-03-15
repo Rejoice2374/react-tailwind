945bba6937b343b732f580c5fc930e22b546bbb5
Rejoice2374 <adeboyerejoice2374@gmail.com>
2023-03-15 23:26:42 +0100

    navbar correction

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,6 @@
 const CTA = () => {
   return (
-    <section id="cta" class="bg-brightRed">
+    <section id="cta" className="bg-brightRed">
       <div className="container flex flex-col items-center justify-between px-6 py-12 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0">
         <h2 className="text-5xl font-bold leading-tight text-center text-white md:text-4xl md:max-w-xl md:text-left">
           Simplify how your team works today
